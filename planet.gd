745d0760bb0facfbf91421aced9ac1a7d88cef28
ShadowCurse <48793978+ShadowCurse@users.noreply.github.com>
2024-08-17 18:41:10 +0100

    camera smoothing + zoom in/out

--- planet.gd
+++ planet.gd
@@ -2,14 +2,23 @@ extends Node3D
 class_name Planet
 
 @export var resource_point_scene: PackedScene
+@export var axis_rotation_speed: float = 0.1
+@export var camera_speed: float = 0.005 
+@export var camera_follow_speed: float = 40.0
+@export var camera_zoom_speed: float = 0.5
+@export var camera_distance: float = 10.0
+@export var camera_max_distance: float = 50.0
+@export var camera_min_distance: float = 6.0
 
 @onready var planet_mesh: MeshInstance3D = $planet_mesh
 @onready var camera_3d: Camera3D = $Camera3D
 @onready var debug_cursor = $debug_cursor
 
 var planet_radius: float = 0.5
-var axis_rotation_speed: float = 0.1
 var mouse_input: Vector2
+
+var camera_theta: float = 0.0
+var camera_phi: float = 0.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -21,24 +30,19 @@ func _process(delta):
 	self.rotate_y(axis_rotation_speed * delta)
 	
 	# update camera
-	var to_camera = camera_3d.position - planet_mesh.position
-	var r = to_camera.length()
-	var theta = acos(to_camera.y / r)
-	var phi = sign(to_camera.x) * acos(to_camera.z / sqrt(to_camera.z * to_camera.z + to_camera.x * to_camera.x))
-	phi += mouse_input.x * 0.01
-	theta += mouse_input.y * 0.01
+	camera_phi -= mouse_input.x * camera_speed
+	camera_theta -= mouse_input.y * camera_speed
 	# clamp Y rotation
-	if theta < 0.1:
-		theta = 0.1
-	if theta > PI - 0.1:
-		theta = PI - 0.1
-	var new_z = r * sin(theta) * cos(phi)
-	var new_x = r * sin(theta) * sin(phi)
-	var new_y = r * cos(theta)
+	if camera_theta < 0.1:
+		camera_theta = 0.1
+	if camera_theta > PI - 0.1:
+		camera_theta = PI - 0.1
+	var new_z = camera_distance * sin(camera_theta) * cos(camera_phi)
+	var new_x = camera_distance * sin(camera_theta) * sin(camera_phi)
+	var new_y = camera_distance * cos(camera_theta)
 	var new_position = Vector3(new_x, new_y, new_z)
-	camera_3d.position = new_position
+	camera_3d.position = new_position + (camera_3d.position - new_position) * exp(-camera_follow_speed * delta)
 	camera_3d.look_at(planet_mesh.global_position)
-	
 	mouse_input = Vector2.ZERO
 	
 	var mouse_pos = camera_3d.get_viewport().get_mouse_position()
@@ -69,8 +73,18 @@ func _process(delta):
 			self.add_child(resource_point)
 
 func _unhandled_input(event)-> void:
+	if Input.is_mouse_button_pressed(MOUSE_BUTTON_WHEEL_UP):
+		camera_distance -= 0.5
+		if camera_distance < camera_min_distance:
+			camera_distance = camera_min_distance
+	if Input.is_mouse_button_pressed(MOUSE_BUTTON_WHEEL_DOWN):
+		camera_distance += 0.5
+		if camera_max_distance < camera_distance:
+			camera_distance = camera_max_distance
+
 	if Input.is_mouse_button_pressed(MOUSE_BUTTON_MIDDLE):
 		if event is InputEventMouseMotion:
 			var viewport_transform: Transform2D = get_tree().root.get_final_transform()
 			mouse_input += event.xformed_by(viewport_transform).relative
+			
 
