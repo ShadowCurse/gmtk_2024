b631e1a6fd797f9aaed9862626e1c4eec16ff788
ShadowCurse <48793978+ShadowCurse@users.noreply.github.com>
2024-08-17 18:49:32 +0100

    add hub

---
+++ hub.gd
@@ -0,0 +1,23 @@
+extends Node3D
+class_name ResourceHub
+
+@onready var mesh_instance_3d = $MeshInstance3D
+
+enum Type {
+	Red,
+	Green,
+	Blue,
+}
+
+@export var type: Type = Type.Red
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	match type:
+		Type.Red: mesh_instance_3d.get_surface_override_material(0).albedo_color = Color.RED
+		Type.Green: mesh_instance_3d.get_surface_override_material(0).albedo_color = Color.GREEN
+		Type.Blue: mesh_instance_3d.get_surface_override_material(0).albedo_color = Color.BLUE
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
