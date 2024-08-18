dee533b4c408000f74d1a190fe54d39d4ab2d498
ShadowCurse <48793978+ShadowCurse@users.noreply.github.com>
2024-08-18 21:14:18 +0100

    rotate sun around the planet

--- solar_system.gd
+++ solar_system.gd
@@ -5,8 +5,8 @@ extends Node3D
 @onready var directional_light_3d = $DirectionalLight3D
 
 var planet_distance: float = 1000.0
-var planet_rotation: float = 0.0
-var planet_rotation_speed: float = 0.0
+var planet_rotation: float = PI / 2.0
+var planet_rotation_speed: float = 0.1
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -15,7 +15,7 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-    #planet_rotation += planet_rotation_speed * delta
-    #planet.position = Vector3(sin(planet_rotation) * planet_distance, 0.0, cos(planet_rotation) * planet_distance)
-    #directional_light_3d.look_at(planet.global_position)
+    planet_rotation += planet_rotation_speed * delta
+    sun.position = Vector3(sin(planet_rotation) * planet_distance, 0.0, cos(planet_rotation) * planet_distance)
+    sun.look_at(planet.global_position)
     pass
