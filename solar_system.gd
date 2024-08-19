119c03591b962e8246c6b05636eea9e5bc2af2dd
ShadowCurse <48793978+ShadowCurse@users.noreply.github.com>
2024-08-19 23:15:46 +0100

    add env to solar system + rotate sky

--- solar_system.gd
+++ solar_system.gd
@@ -2,10 +2,11 @@ extends Node3D
 
 @onready var planet = $planet
 @onready var sun = $sun
+@onready var world_environment = $WorldEnvironment
 
 var planet_distance: float = 1000.0
 var planet_rotation: float = PI / 2.0
-var planet_rotation_speed: float = 0.1
+var planet_rotation_speed: float = 0.05
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -17,4 +18,5 @@ func _process(delta):
     planet_rotation += planet_rotation_speed * delta
     sun.position = Vector3(sin(planet_rotation) * planet_distance, 0.0, cos(planet_rotation) * planet_distance)
     sun.look_at(planet.global_position)
+    world_environment.environment.sky_rotation.y += planet_rotation_speed * delta
     pass
