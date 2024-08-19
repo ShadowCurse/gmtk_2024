d58298168901c7dcdb6acefe7a50097c1d60b974
ShadowCurse <48793978+ShadowCurse@users.noreply.github.com>
2024-08-19 21:30:50 +0100

    add basic ui

---
+++ ui.gd
@@ -0,0 +1,20 @@
+extends Control
+
+@onready var red_count = $VBoxContainer/HBoxContainer/red/HBoxContainer/red_count
+@onready var green_count = $VBoxContainer/HBoxContainer/green/HBoxContainer/green_count
+@onready var blue_count = $VBoxContainer/HBoxContainer/blue/HBoxContainer/blue_count
+
+func _ready():
+    pass
+
+func _process(delta):
+    pass
+
+func update_red_count(count: int):
+    self.red_count.text = "%d" % count
+
+func update_green_count(count: int):
+    self.green_count.text = "%d" % count
+
+func update_blue_count(count: int):
+    self.blue_count.text = "%d" % count
